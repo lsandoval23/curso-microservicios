f16db03d6c756eb406d7299d7dc945902bb77cb7
Luis Sandoval <lsandovalancco@gmail.com>
2023-06-23 13:34:25 -0500

    Clase 1: Configurando el discovery server

---
+++ discovery-server/src/main/java/com/lsandoval/microservices/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package com.lsandoval.microservices.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// El servidor necesita la anotacion Enable Eureka Server para que pueda levantar.
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}
