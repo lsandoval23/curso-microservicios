978546492fdf17d200a08718619d0c521ce3cbf3
Luis Sandoval <lsandovalancco@gmail.com>
2023-06-24 20:18:53 -0500

    Clase 2: Config-server Creamos el nuevo módulo de config-server con sus configuraciones iniciales. Previamente creando el repositorio https://github.com/lsandoval23/config-server-files.git

---
+++ config-server/src/main/java/com/lsandoval/microservices/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.lsandoval.microservices.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+// El servicio necesita la anotación enable config server
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
