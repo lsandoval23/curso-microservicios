eaf9934b8abb6939c04e475c8af9c28a62b56a87
Luis Sandoval <lsandovalancco@gmail.com>
2023-07-08 19:49:11 -0500

    Configuramos querys y añadimos paginación

--- user-service/src/main/java/com/lsandoval/microservices/userservice/service/repository/UserRepository.java
+++ user-service/src/main/java/com/lsandoval/microservices/userservice/service/repository/UserRepository.java
@@ -2,12 +2,26 @@ package com.lsandoval.microservices.userservice.service.repository;
 
 import com.lsandoval.microservices.userservice.model.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 // Anotacion de spring data rest, expone a userEntity y su CRUD como endpoints http, ademas de registrarlo como bean
 // Ya no es necesario anotarlo con la anotacion @Repository
 
 @RepositoryRestResource(path = "user")
-public interface UserRepository extends CrudRepository<UserEntity, String> {
+public interface UserRepository extends CrudRepository<UserEntity, String>, PagingAndSortingRepository<UserEntity, String> {
 
+    // Podemos definir nuevos endpoints, para querys específicos, en este caso para obtener users en base al email
+    @RestResource(path = "email")
+    List<UserEntity> getAllByEmail(String email);
+
+    @RestResource(path = "role")
+    List<UserEntity> getAllByRoles(String roles);
+
+    // De esta manera bloqueamos el endpoint para poder eliminar elementos.
+    @RestResource(exported = false)
+    void deleteById(String id);
 }
