a45ee1fa51cb5fa6d12432f4cb59a49437ba0ee8
Luis Sandoval <lsandovalancco@gmail.com>
2023-07-10 21:16:15 -0500

    - Añadimos un filtro custom, este se especifica en el archivo de configuraciones hacia que rutas se aplican. - Añadimos logs

---
+++ cloud-gateway/src/main/java/com/lsandoval/microservices/cloudgateway/config/CustomFilter.java
@@ -0,0 +1,46 @@
+package com.lsandoval.microservices.cloudgateway.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.CustomConfiguration> {
+
+    public CustomFilter(){
+        super(CustomConfiguration.class);
+    }
+
+
+    @Override
+    public GatewayFilter apply(CustomConfiguration config) {
+        return ((exchange, chain) -> {
+
+            // Prefilter
+            log.info("[CustomFilter] - [apply]: Pre filter");
+            exchange.getRequest().mutate().header("callerName", config.headerValue);
+
+            return chain.filter(exchange).then( Mono.fromRunnable( () -> {
+                // PostFilter
+
+                exchange.getResponse().getCookies().add("RESPONSE", ResponseCookie.from(config.headerKey, config.headerValue).build());
+                exchange.getResponse().getHeaders().add("RESPONSE", config.headerValue);
+
+                log.info("HeaderKey: " + config.headerKey);
+                log.info("HeaderValue: " + config.headerValue);
+                log.info("[CustomFilter] - [apply]: Post filter");
+            }));
+        });
+    }
+
+    @Data
+    public static class CustomConfiguration {
+        private String headerKey;
+        private String headerValue;
+    }
+}
--- cloud-gateway/src/main/java/com/lsandoval/microservices/cloudgateway/config/GlobalFilters.java
+++ cloud-gateway/src/main/java/com/lsandoval/microservices/cloudgateway/config/GlobalFilters.java
@@ -1,5 +1,6 @@
 package com.lsandoval.microservices.cloudgateway.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.ResponseCookie;
@@ -8,12 +9,15 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 // Implementamos un filtro global que afectara a todas las solicitudes que vayan a Cloud Gateway
+@Slf4j
 @Component
 public class GlobalFilters implements GlobalFilter {
 
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        log.info("[GlobalFilters] - [filter]: Pre filter");
 
         // Pre filter: Antes de que vaya al microservicio correspondiente agregamos un header con el nombre callerName
         String callerName = "Cloud_Gateway";
@@ -23,6 +27,7 @@ public class GlobalFilters implements GlobalFilter {
 
             // Post Filter: A la respuesta que nos devuelve el microservicio le añadimos un cookie y un header antes de
             //              devolver la respuesta
+            log.info("[GlobalFilters] - [filter]: Post filter");
             exchange.getResponse().getCookies().add("RESPONSE", ResponseCookie.from("CALLER_NAME", callerName).build());
             exchange.getResponse().getHeaders().add("RESPONSE", callerName);
 
