379b28e235f0d108e833d201aa38ad420e0eefb3
Luis Sandoval <lsandovalancco@gmail.com>
2023-09-04 00:59:52 -0500

    - Añadimos la dependencia de circuito breaker al modulo de client-service, ya que este hace peticiones a otros microservicios. - Agregamos el circuito en la capa de servicio.

--- client-service/src/main/java/com/lsandoval/microservices/clientservice/service/ClientService.java
+++ client-service/src/main/java/com/lsandoval/microservices/clientservice/service/ClientService.java
@@ -6,25 +6,57 @@ import com.lsandoval.microservices.clientservice.proxy.openfeign.ProductFeign;
 import com.lsandoval.microservices.clientservice.proxy.resttemplate.CloudGatewayRestTemplate;
 import com.lsandoval.microservices.clientservice.proxy.resttemplate.ProductRestTemplate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ClientService {
 
     // private final ProductRestTemplate productRestTemplate;
     //private final CloudGatewayRestTemplate cloudGatewayRestTemplate;
-    private final ProductFeign productFeign;
-    // private final CloudGatewayFeign cloudGatewayFeign;
+    // private final ProductFeign productFeign;
+
+    private final CloudGatewayFeign cloudGatewayFeign;
+    // Clase que nos permite generar circuitos para cada request.
+    private final CircuitBreakerFactory circuitBreakerFactory;
+
+    /*public List<ProductDTO> getAllProducts(){
+        return cloudGatewayFeign.getAllProducts();
+    }*/
 
     public List<ProductDTO> getAllProducts(){
-        return productFeign.getAllProducts();
+
+        // El id es el nombre que se le da al circuito, el metodo run acepta el camino del circuito cerrado y el fallback como parametros
+        // En este caso, el primero es cloudGatewayFeign.getAllProducts() y el segundo getAlternativeProducts()
+        return circuitBreakerFactory.create("mitocode")
+                .run( () -> cloudGatewayFeign.getAllProducts() ,
+                      e ->  getAlternativeProducts(e) );
     }
 
     public ProductDTO saveProduct(ProductDTO productDTO){
-        return productFeign.saveProduct(productDTO);
+        return cloudGatewayFeign.saveProduct(productDTO);
+    }
+
+    private List<ProductDTO> getAlternativeProducts(Throwable e){
+
+        log.info(e.getMessage());
+        List<ProductDTO> lstProducts = new ArrayList<>();
+        ProductDTO productDTO = ProductDTO.builder()
+                .productId("P9999")
+                .productName("Product Fake")
+                .productType("Fake")
+                .stock(5)
+                .build();
+
+        lstProducts.add(productDTO);
+
+        return  lstProducts;
     }
 
 
