153090c09ed4180065b2986ef9708018066bfda8
Luis Sandoval <lsandovalancco@gmail.com>
2023-09-13 17:41:39 -0500

    Clase 7: Circuit Breaker, añadimos una clase para configurar los parámetros del circuito.

---
+++ client-service/src/main/java/com/lsandoval/microservices/clientservice/config/CircuitBreakerCustomConfig.java
@@ -0,0 +1,31 @@
+package com.lsandoval.microservices.clientservice.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CircuitBreakerCustomConfig {
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> overrideDefaultConfig(){
+        return factory -> factory.configureDefault(id -> {
+            return new Resilience4JConfigBuilder(id)
+                    .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                            .slidingWindowSize(15)      //  Default 100
+                            .failureRateThreshold(30)   // Default 50%
+                            .waitDurationInOpenState(Duration.ofSeconds(20L))       // Default 60s
+                            .permittedNumberOfCallsInHalfOpenState(10)              // Default 10
+                            .slowCallDurationThreshold(Duration.ofMillis(1500L))    // Default 1s
+                            .slowCallRateThreshold(10)
+                            .build()).build();
+        });
+    }
+
+
+}
